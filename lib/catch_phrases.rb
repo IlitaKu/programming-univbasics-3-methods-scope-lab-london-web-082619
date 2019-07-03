db1c7869c188a0b8390a54ccb65346f622038938
Ilita Kunelyte <kunelyte.ilita90@gmail.com>
2019-07-03 14:00:18 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -7,4 +7,9 @@ def toadstool
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
-mario
+def link
+  puts "It's Dangerous To Go Alone! Take This"
+end
+def any_phrase (phrase)
+puts phrase
+end
